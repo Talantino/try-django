6c5c02f8eb40e0136ae09c8103e19c2e5214f455
Talant Kosmosbekov <t1kosmos1@gmail.com>
2024-02-26 23:54:27 +0600

    Created login view and accounts app

--- trydjango/urls.py
+++ trydjango/urls.py
@@ -16,13 +16,20 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from .views import home_view
-from articles import views
+from articles.views import (
+    article_search_view,
+    article_create_view,
+    article_detail_view,
+)
+from accounts.views import login_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", home_view),
-    path('articles/', views.article_search_view),
-    path('articles/create/', views.article_create_view),
-    path('articles/<int:id>/', views.article_detail_view),
+    path('articles/', article_search_view),
+    path('articles/create/', article_create_view),
+    path('articles/<int:id>/', article_detail_view),
     #re_path(r'articles/(?P<id>\d+)/$', home_view),
+    path('login/', login_view),
+
 ]
